ddb300321792f6cbf5d8df87c53ac025e61f760d
gabriel-williams <gabrielwilliamcps@gmail.com>
2022-09-13 20:18:07 -0300

    mobile: aula 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,50 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter";
+
+import { Background } from "./src/components/Background";
+import { Home } from "./src/screens/Home/inde";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello world!</Text>
-      <StatusBar style="auto" backgroundColor="#006060" />
-      <Button title="Cadastrar" />
-      <Button title="Cadastrar" />
-      <Button title="Cadastrar" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity style={styles.buttons}>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#5be4ff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  title: {
-    color: "#fff",
-    fontSize: 22
-  },
-
-  buttons: {
-    width: "50%",
-    height: "5%",
-    borderRadius: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f0f",
-    marginTop: "5%"
-  }
-});
